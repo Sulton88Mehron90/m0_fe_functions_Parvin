55ed2aae12d3a18b48939b3e646e98e384b51af9
Parvin Sattorova <nanajon66@gmail.com>
2022-12-10 23:37:24 -0500

    Initial commit

--- cleanUp.js
+++ cleanUp.js
@@ -4,29 +4,68 @@
   // Modify the code to make that improvement(s)
   // write a JS comment to explain what you changed, and why
   // make sure the code you submit WORKS - you can run this entire file 
-    // using `node cleanUp.js` or copy and paste into a replit
+  // using `node cleanUp.js` or copy and paste into a replit
 
 
 // EX 1:
-function nameQuestion() {
-  console.log("Hello, what is your name?")
+// function nameQuestion() {
+//   console.log("Hello, what is your name?")
+// }
+
+// nameQuestion()
+
+
+
+function nameQuestion(name) {
+  console.log("Hello, what is your name?");
+  console.log(`Hello, my name is ${name}`);
+  console.log(`Nice to meet you, ${name}`);
 }
 
-nameQuestion()
+nameQuestion("Parvin")
+
+// //  --> Declared parameter `name` in function definition.
+// //  --> Add interpolation `${name}` in `console.log` and in function call as argument.
+// //  --> Add two more `console log`.
 
 
 // EX 2:
+
+// function addThreeNums(first, second, third) {
+// var sum = first + second + third
+// console.log(sum);
+//       }
+
+// addThreeNums(1, 2, 3);
+// addThreeNums(4, 2, 7);
+
+
+
 function addThreeNums(first, second, third) {
-var sum = first + second + third
-console.log(sum);
+  var sum = first + second + third
+  console.log (`${sum} is the answer`);
       }
+  
+  addThreeNums(1, 2, 3)
+  addThreeNums(4, 2, 7);
 
-addThreeNums(1, 2, 3);
-addThreeNums(4, 2, 7);
+// //  --> Add interpolation `${sum}` and a text in `console.log`
 
 
-// EX 3:
-func makeFreshPesto(){
+// // EX 3:
+
+// func makeFreshPesto(){
+//   console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
+//   console.log("Pulse basil and pine nuts");
+//   console.log("Add garlic and cheeses");
+//   console.log("Slowly pour in oil");
+//   console.log("Season");    }
+
+// makeFreshPesto();
+
+
+
+function makeFreshPesto(){
   console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
   console.log("Pulse basil and pine nuts");
   console.log("Add garlic and cheeses");
@@ -35,12 +74,31 @@ func makeFreshPesto(){
 
 makeFreshPesto();
 
+// //  --> function was misspelled. corrected from func to function.
+
 
 //  EX 4:
+// function average(num1, num2) 
+//   {
+// var sum = num1 + num2;
+//     var avg = sum / 2;
+
+//   console.log(`the average is: ${avg}`.)
+//   }
+
+
+
 function average(num1, num2) 
   {
 var sum = num1 + num2;
     var avg = sum / 2;
 
-  console.log(`the average is: ${avg}`.)
+  console.log(`the average is: ${avg}.`)
   }
+average(5,6)
+
+// //  --> add Function Call (or Invoke) and moved the dot incide the backtick in console.
+
+
+
+
--- functions.js
+++ functions.js
@@ -1,22 +1,75 @@
 // 1: Write a function named printGreeting that prints a simple greeting message, the same one, every time it is called. Call this function 3 times.
 
+function printGreeting(name) {
+    console.log(`Hello ${name}`);
+  }
+  
+  printGreeting("Parvin")
+  printGreeting("Sulton")
+  printGreeting("Mehron")
+
 
 // 2: Write a function that accepts 1 argument. The function should console.log a sentence that interpolates the data passed in.
 
+function argument(num1, num2){
+var sum = num1 + num2;
+    console.log(`${sum} is the answer`);
+}
+argument(2, 2)
+argument(1, 1)
 
-// 3: Write a function that has 3 parameters: a string and two numbers. The String will be the name of a company, and the numbers will represent the minimum and maximum of a pay range for a posted job. The function should print out a sentence that includes the name of the company and the range itself (if the numbers passed in are 90000 and 110000, the pay range is 20000).
+// 3: Write a function that has 3 parameters: a string and two numbers. 
+// The String will be the name of a company, and the numbers will represent the minimum and maximum of a pay range for a posted job. 
+// The function should print out a sentence that includes the name of the company and the range itself 
+// (if the numbers passed in are 90000 and 110000, the pay range is 20000).
+
+// => The basic formula of average is the sum of all the values divided by the total number of values.
+
+function postedJob(Value1, Value2, EchoStar) {
+
+var numberOfValue = 2
+var sum = Value1 + Value2;
+var div = sum /= numberOfValue;
+
+console.log(`Job post for a position in EchoStar company. The minimum and maximum of a pay range for a posted job are 
+${Value1} and ${Value2}. The pay range is: $ ${div}`);
+
+}
+
+postedJob(70000, 110000);
 
 
 // 4: Write a function that satifies the following interaction pattern:
 
-checkStock(4, "Coffee");
-// => "Coffee is stocked"
+// checkStock(4, "Coffee");
+// // => "Coffee is stocked"
 
-checkStock(3, "Tortillas");
-// => "Tortillas - running LOW"
+function checkStock(num, Coffee) {
+console.log("Coffee is stocked");
+}
+checkStock("4")
 
-checkStock(0, "Cheese");
-// => "Cheese - OUT of stock!"
+// checkStock(3, "Tortillas");
+// // => "Tortillas - running LOW"
 
-checkStock(1, "Salsa");
-// => "Salsa - running LOW"
+function checkStock (num, Tortillas) {
+console.log("Tortillas - running LOW")
+}
+checkStock(3);
+
+// checkStock(0, "Cheese");
+// // => "Cheese - OUT of stock!"
+
+function checkStock(num, Cheese){
+console.log("Cheese - OUT of stock!");
+}
+checkStock(0);
+
+// checkStock(1, "Salsa");
+// // => "Salsa - running LOW"
+
+function checkStock(num, Salsa){
+console.log("Salsa - running LOW");
+}
+checkStock(1);
+
